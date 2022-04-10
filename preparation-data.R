8765b8664e93578e3c36c8252ec1b14266c9e2a1
Francesca.do <96070459+Fradovidio@users.noreply.github.com>
2022-04-10 15:28:04 +0200

    Update and rename preparation-data to preparation-data.R
    Code pt. 1. 
    Upload entire dataset.
    Create matrix

--- preparation-data.R
+++ preparation-data.R
@@ -1,11 +1,11 @@
 rm(list=ls())
 
 
-#setwd("C:/Users/(...)/File Excel S&P") #use your path
+#setwd("C:/Users/(...)/File Excel S&P") 
 
 dati1927 =read.csv("serie1927.csv") # Historical yahoo data  
 
-## Create matrix with series with lags (in this case is just for one year (252 days) 
+## create a matrix with the lagged series (in this case is just for one year (252 days) 
 
 dati_agg_H = matrix(0,504,504) # Raw data
 dati_agg_L = matrix(0,504,504) 
@@ -17,9 +17,10 @@ dati_diff_logH = matrix(0,503,504) # difference between log prices: log(Pt)- log
 dati_diff_logL = matrix(0,503,504) #  This one has one less record
 
 ### Create matrix
+
 for (i in 1:504)
 {
-  ## Fro 19 sept. 2016 to 19 sept 2018; 
+  ## From 19 sept. 2016 to 19 sept 2018; 
   # if you add 504 obs to it on Sept. 19, 2018, you get to Sept. 20, 2020
   dati_agg_H[,i] = dati1927[(22284+i):(22787+i),3] 
   dati_agg_L[,i] = dati1927[(22284+i):(22787+i),4]
@@ -27,7 +28,7 @@ for (i in 1:504)
   dati_agg_log_H[,i]= log(dati_agg_H[,i])
   dati_agg_log_L[,i]= log(dati_agg_L[,i]) 
   
-  dati_diff_logH[,i] = diff(dati_agg_log_H[,i]) # Per le differenze, io ottengo, un record in meno 
+  dati_diff_logH[,i] = diff(dati_agg_log_H[,i]) # we're gonna have a record less for the differences
   dati_diff_logL[,i] = diff(dati_agg_log_L[,i])
 }
 
